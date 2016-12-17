16c9c86fb34c8150b8c86529328aa18d8ebd465b
Eddie <ejds001@gmail.com>
2016-12-17 12:00:58 -0800

    light_switches.rb

--- exercises/light_switches.rb
+++ exercises/light_switches.rb
@@ -26,15 +26,64 @@
 #     -increment i by 1
 # -return array
 
-def lightsOn(n)
-  return "Invalid input, n cannot be negative!" if n < 0 
-  on_arr = [] 
-  last_perfect_square_root = 1
-  while last_perfect_square_root**2 <= n
-    on_arr << last_perfect_square_root**2 
-    last_perfect_square_root += 1
+#--------------------------------------------------------------------------------------
+#Second Version
+
+#def lightsOn(n)
+#  return "Invalid input, n cannot be negative!" if n < 0 
+#  on_arr = [] 
+#  last_perfect_square_root = 1
+#  while last_perfect_square_root**2 <= n
+#    on_arr << last_perfect_square_root**2 
+#    last_perfect_square_root += 1
+#  end
+#  on_arr
+#end
+
+# Pseudo 2
+# initialize on_arr as array of 0s to signal off
+# set pass = 0
+# while pass less than or equal to n
+  # toggle(change to 1 in this case) a given indice if the indice + 1(because of zero
+  # indexing) is a multiple of pass
+  # increment pass by 1
+# return on_arr
+
+def is_multiple?(num1, num2)
+  num1 % num2 == 0
+end
+
+def select_on_lights(light_arr)
+  on_arr = []
+  light_arr.each_with_index do |light, index|
+    if light == 1
+      on_arr << index + 1
+    end
   end
   on_arr
+end
+
+def toggle_light(light_arr, index)
+  if light_arr[index] == 0
+    light_arr[index] = 1
+  else
+    light_arr[index] = 0
+  end
+end
+
+def lightsOn(n)
+  return "Invalid input, n cannot be negative!" if n < 0 
+  light_arr = Array.new(n, 0)  
+  pass = 1
+  while pass <= n
+    light_arr.each_index do |index|
+      if is_multiple?(index + 1, pass) 
+        toggle_light(light_arr, index)
+      end
+    end
+    pass += 1
+  end
+  select_on_lights(light_arr)
 end
 
 p "lightsOn(5): #{lightsOn(5)}" 
